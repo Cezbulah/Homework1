1e2d3b1e2c8e3d634516f46243476f878193382d
Cezbulah <62251652+Cezbulah@users.noreply.github.com>
2020-03-17 18:06:28 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,6 +1,7 @@
 from typing import List
-
+import datetime
 import pandas as pd
+
 
 CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
                       f"/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "
@@ -15,33 +16,35 @@ confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
 def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
     """
     Returns confirmed infection cases for country 'Poland' given a date.
-
     Ex.
     >>> poland_cases_by_date(7, 3, 2020)
     5
     >>> poland_cases_by_date(11, 3)
     31
-
     :param year: 4 digit integer representation of the year to get the cases for, defaults to 2020
     :param day: Day of month to get the cases for as an integer indexed from 1
     :param month: Month to get the cases for as an integer indexed from 1
     :return: Number of cases on a given date as an integer
     """
     
-    # Your code goes here (remove pass)
-    pass
+    # Your code goes here (remove pass)   
+    url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    df = pd.read_csv(url, error_bad_lines=False)
+    year-=2000
+    result = df.loc[df["Country/Region"]=="Poland"][f"{month}/{day}/{year}"].values[0]
+    return result
 
+print(poland_cases_by_date(7,3,2020))
+print(poland_cases_by_date(11,3))
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     """
     Returns the top 5 infected countries given a date (confirmed cases).
-
     Ex.
     >>> top5_countries_by_date(27, 2, 2020)
     ['China', 'Korea, South', 'Cruise Ship', 'Italy', 'Iran']
     >>> top5_countries_by_date(12, 3)
     ['China', 'Italy', 'Iran', 'Korea, South', 'France']
-
     :param day: 4 digit integer representation of the year to get the countries for, defaults to 2020
     :param month: Day of month to get the countries for as an integer indexed from 1
     :param year: Month to get the countries for as an integer indexed from 1
@@ -49,20 +52,27 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     """
 
     # Your code goes here (remove pass)
-    pass
+    url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    df = pd.read_csv(url, error_bad_lines=False)
+    year -= 2000
+    data = f"{month}/{day}/{year}"
+    grouped = confirmed_cases.groupby("Country/Region", as_index=False).sum(level=0)
+    sort_all = grouped.sort_values(by=data, ascending = False)
+    top5_countries = list(sort_all["Country/Region"].values[:5])
+    return top5_countries
+print(top5_countries_by_date(27,2,2020)) 
+print(top5_countries_by_date(12,3)) 
 
 # Function name is wrong, read the pydoc
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     """
     Returns the number of countries/regions where the infection count in a given day
     was NOT the same as the previous day.
-
     Ex.
     >>> no_new_cases_count(11, 2, 2020)
     35
     >>> no_new_cases_count(3, 3)
     57
-
     :param day: 4 digit integer representation of the year to get the cases for, defaults to 2020
     :param month: Day of month to get the countries for as an integer indexed from 1
     :param year: Month to get the countries for as an integer indexed from 1
@@ -70,4 +80,14 @@ def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     """
     
     # Your code goes here (remove pass)
-    pass
+    url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    df = pd.read_csv(url, error_bad_lines=False)
+    year -= 2000
+    data_teraz = f"{month}/{day}/{year}"
+    d = datetime.date(year,month,day)
+    wczoraj = d+datetime.timedelta(days=-1)
+    wczoraj_string = f"{wczoraj.month}/{wczoraj.day}/{wczoraj.year}"
+    wynikowo = confirmed_cases.loc[confirmed_cases[data_teraz]!=confirmed_cases[wczoraj_string]]
+    return wynikowo.shape[0]
+print(no_new_cases_count(11, 2, 2020))
+print(no_new_cases_count(3,3))
