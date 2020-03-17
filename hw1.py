648b8f1f0a81011258ba076fc900498789269dca
Cezbulah <62251652+Cezbulah@users.noreply.github.com>
2020-03-17 20:52:29 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -77,6 +77,9 @@ def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     """
     
     # Your code goes here (remove pass)
+    CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
+                      f"/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "
+    confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
     year=20
     data_teraz = f"{month}/{day}/{year}"
     d = datetime.date(year,month,day)
