36f9dda5751de8a183d28440bed8cae0c30e3908
Cezbulah <62251652+Cezbulah@users.noreply.github.com>
2020-03-17 19:44:06 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -52,8 +52,8 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     """
 
     # Your code goes here (remove pass)
-    url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-    df = pd.read_csv(url, error_bad_lines=False)
+    CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "    
+    confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
     year-=2000
     data = f"{month}/{day}/{year}"
     grouped = confirmed_cases.groupby("Country/Region", as_index=False).sum(level=0)
