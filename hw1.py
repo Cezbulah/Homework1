4afb0ae12e95c8758385b472de2b15d06fd38f93
Cezbulah <62251652+Cezbulah@users.noreply.github.com>
2020-03-17 23:25:07 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,18 +1,18 @@
+from typing import List
 import datetime
 import pandas as pd
-import List from typing
-​
-​
+
+
 CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
                       f"/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "
-​
+
 """
 When downloading data it's better to do it in a global scope instead of a function.
 This speeds up the tests significantly
 """
 confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
-​
-​
+
+
 def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
     """
     Returns confirmed infection cases for country 'Poland' given a date.
@@ -33,23 +33,23 @@ def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
     year-=2000
     result = df.loc[df["Country/Region"]=="Poland"][f"{month}/{day}/{year}"].values[0]
     return int(result)
-​
-​
-print(poland_cases_by_date(7,3,2020))
+
+
+
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     """
     Returns the top 5 infected countries given a date (confirmed cases).
     Ex.
-    >>> top5_countries_by_date(27, 2, 2020)
+    top5_countries_by_date(27, 2, 2020)
     ['China', 'Korea, South', 'Cruise Ship', 'Italy', 'Iran']
-    >>> top5_countries_by_date(12, 3)
+    top5_countries_by_date(12, 3)
     ['China', 'Italy', 'Iran', 'Korea, South', 'France']
     :param day: 4 digit integer representation of the year to get the countries for, defaults to 2020
     :param month: Day of month to get the countries for as an integer indexed from 1
     :param year: Month to get the countries for as an integer indexed from 1
     :return: A list of strings with the names of the coutires
     """
-​
+
     # Your code goes here (remove pass)
     year_fix = year - 2000
     grouped = confirmed_cases.groupby("Country/Region", as_index=False).sum()
@@ -57,9 +57,8 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     top5_countries = sort_all["Country/Region"].tolist()
     result = top5_countries[:5]
     return result
-​
-print(top5_countries(27,2,2020))
-​
+
+
 # Function name is wrong, read the pydoc
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     """
@@ -77,12 +76,10 @@ def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     """
     
     # Your code goes here (remove pass)
-    year-=2000
+    year=20
     data_teraz = f"{month}/{day}/{year}"
     d = datetime.date(year,month,day)
     wczoraj = d+datetime.timedelta(days=-1)
     wczoraj_string = f"{wczoraj.month}/{wczoraj.day}/{wczoraj.year}"
     wynikowo = confirmed_cases.loc[confirmed_cases[data_teraz]!=confirmed_cases[wczoraj_string]]
     return int(wynikowo.shape[0])
-​
-print(no_new_cases_count(11,2,2020))
